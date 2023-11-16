8d9c5b92cafecb760aff0ded62f4bb6250b8a24a
tbidault <tristanbidault@gmail.com>
2023-11-16 10:09:53 +0100

    1st local

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,12 @@
-import './App.css';
-
+import "./App.css";
+import  Form  from "./Components/Form/form";
 function App() {
   return (
-  <h1>Les plantes verte de couleur verte</h1>
-)}
+    <>
+      <h1>Les plantes verte de couleur verte</h1>
+      <Form />
+    </>
+  );
+}
 
 export default App;
