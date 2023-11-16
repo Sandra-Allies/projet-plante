18fef1a3a8621ed0e580ad4006a5dcaf9db400c2
Sandra-Allies <sandra.allies.fr@gmail.com>
2023-11-16 11:50:11 +0100

    fichier json pour les data des plantes

--- vite-project/src/Components/PlantList/PlantList.jsx
+++ vite-project/src/Components/PlantList/PlantList.jsx
@@ -1,9 +1,12 @@
 import './PlantList.css';
+import Data from '../../data.json';
+import { Icon } from '@iconify/react';
 
 function PlantList() {
+  // console.log(Data.data[0]);
   return (
     <div className="plantList">
-    
+      {/* <img src={Data.data[0].picture} /> */}
     </div>
 )}
 
