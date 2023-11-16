0613ee1ff9a17ba0d298a51cc7045e715a323812
tbidault <tristanbidault@gmail.com>
2023-11-16 15:30:06 +0100

    des trucs des machins

--- vite-project/src/Components/Form/Form.jsx
+++ vite-project/src/Components/Form/Form.jsx
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Form.css";
 import LoremIpsum from "./LoremIpsum";
 
+const bigData = {
+  firstName: "",
+  newName: "",
+  password: "",
+  mail: "",
+};
+
 const Form = () => {
+  const [data, setData] = useState({
+    firstName: "",
+    newName: "",
+    password: "",
+    mail: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChangeF = (e) => {
+    setData((prev) => ({ ...prev, firstName: e.target.value }));
+    console.log(bigData);
+  };
+  const handleChangeN = (e) => {
+    setData((prev) => ({ ...prev, newName: e.target.value }));
+    console.log(bigData);
+  };
+  const handleChangeP = (e) => {
+    setData((prev) => ({ ...prev, password: e.target.value }));
+    console.log(bigData);
+  };
+  const handleChangeM = (e) => {
+    setData((prev) => ({ ...prev, mail: e.target.value }));
+    console.log(bigData);
+  };
+
   return (
     <>
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
             className="inputs"
             id="fName"
+            onChange={handleChangeF}
+            value={data.firstName}
             placeholder="First Name"
             required="required"
           />
@@ -18,6 +55,8 @@ const Form = () => {
             type="text"
             className="inputs"
             id="nName"
+            onChange={handleChangeN}
+            value={data.newNameName}
             placeholder="New Name"
             required="required"
           />
@@ -25,7 +64,9 @@ const Form = () => {
             type="password"
             className="inputs"
             id="pass"
-            minlength="6"
+            onChange={handleChangeP}
+            value={data.password}
+            minLength="6"
             placeholder="Password"
             required
           />
@@ -33,17 +74,19 @@ const Form = () => {
             type="email"
             className="inputs"
             id="mail"
+            onChange={handleChangeM}
+            value={data.mail}
             placeholder="Email Address"
             required="required"
           />
           <div id="checkBoxBlock">
             <div id="newsletter">
               <input className="checkB" name="newsletter" type="checkbox" />
-              <label for="newsletter">NEWSLETTER</label>
+              <label htmlFor="newsletter">NEWSLETTER</label>
             </div>
             <div id="rules">
               <input className="checkB" name="rules" type="checkbox" />
-              <label for="rules">RULES</label>
+              <label htmlFor="rules">RULES</label>
             </div>
           </div>
           <input type="submit" className="inputs" id="submit" value="Sign in" />
